06508ae28ba386a612ead4c384894459e790ad0d
Aaron Jenkins <97716896+Nycepter@users.noreply.github.com>
2023-10-09 12:04:17 -0500

    Daily Lessons

---
+++ Day 1/Band Name Generator.py
@@ -0,0 +1,4 @@
+Street_Name = input("What is the name of the street you grew up on?")
+Pet_Name = input("What is the name of your favorite pet?")
+Band_Name = (Street_Name + " " + Pet_Name)
+print("Your band name should be " + Band_Name)
