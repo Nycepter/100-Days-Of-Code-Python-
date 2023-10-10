a36c63c7fc4316b54bfc9417255fdcfd257c131e
Aaron Jenkins <97716896+Nycepter@users.noreply.github.com>
2023-10-09 23:50:22 -0500

    Update Tip Calculator.py

--- Day 2/Tip Calculator.py
+++ Day 2/Tip Calculator.py
@@ -4,4 +4,4 @@ SplitWay = int(input("How many people are splitting the bill? "))
 BillWithTip = float(BillTotal + (BillTotal * (Percentage / 100)))
 EndTotal = round(BillWithTip / SplitWay, 2)
 
-print(f"Each person shouls pay: ${EndTotal}")
+print(f"Each person should pay: ${EndTotal}")
