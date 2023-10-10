23a447f14381025e1447d216b18ff8171d6afc1b
Aaron Jenkins <97716896+Nycepter@users.noreply.github.com>
2023-10-09 23:00:24 -0500

    Create BMI Calculator.py

---
+++ Day 2/BMI Calculator.py
@@ -0,0 +1,38 @@
+"""
+
+Instructions
+Write a program that calculates the Body Mass Index (BMI) from a user's weight and height.
+
+BMI Wikipedia Page
+
+The BMI is a measure of someone's weight taking into account their height. e.g. If a tall person and a short person both weigh the same amount, the short person is usually more overweight.
+
+The BMI is calculated by dividing a person's weight (in kg) by the square of their height (in m):
+
+BMI Formula
+
+NOTE: You should convert the bmi to a whole number and print out a whole number in order to pass all the tests. See examples below.
+
+Example Input 1
+1.75
+80
+means: weight = 80 and height = 1.75
+
+Example Output 1
+26
+Since: 80 ÷ (1.75 x 1.75) = 26.122448979591837
+
+Example Input 2
+
+"""
+
+
+height = input("Enter you height in meters. ")
+weight = input("Enter your weight in kilograms. ")
+
+
+heightnum = float(height)
+weightnum = float(weight)
+BMI = int((weightnum / (heightnum * heightnum)))
+# int BMI to get whole number for test cases
+print(BMI)
