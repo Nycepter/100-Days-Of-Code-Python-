33f2cda94ded6e7e068301e416cf9f6bd9d09d59
Aaron Jenkins <97716896+Nycepter@users.noreply.github.com>
2023-10-09 23:48:12 -0500

    Update Tip Calculator.py

--- Day 2/Tip Calculator.py
+++ Day 2/Tip Calculator.py
@@ -1,7 +1,6 @@
-BillTotal = float(input("What is the bill total? "))
+BillTotal = float(input("What is the bill total? $"))
 Percentage = float(input("What percentage would you like to tip? "))
 SplitWay = int(input("How many people are splitting the bill? "))
-
 BillWithTip = float(BillTotal + (BillTotal * (Percentage / 100)))
 EndTotal = round(BillWithTip / SplitWay, 2)
 
