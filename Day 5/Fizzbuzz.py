ce60972675dcfbd7320a97f61582e35f77e538fa
Aaron Jenkins <97716896+Nycepter@users.noreply.github.com>
2023-10-18 16:22:52 -0500

    update

--- Day 5/Fizzbuzz.py
+++ Day 5/Fizzbuzz.py
@@ -9,3 +9,14 @@ for Number in range(1, 101, 1):
         print("Fizz")
     else:
         print(Number)
+
+
+for Number in range(1, 101, 1):
+    output = ""
+    if Number % 3 == 0:
+        output += "Fizz"
+    if Number % 5 == 0:
+        output += "Buzz"
+    if output == "":
+        output = str(Number)
+    print(output)
