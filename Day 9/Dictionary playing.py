72d10e832ed602a67f14ad156018e13c3dad4d4c
Aaron Jenkins <97716896+Nycepter@users.noreply.github.com>
2023-10-23 23:41:30 -0500

    update

---
+++ Day 9/Dictionary playing.py
@@ -0,0 +1,41 @@
+dictionary = {
+    'Aaron': "A cool guy",
+    'Its fun to': "try new things",
+}
+
+dictionary['bingo'] = "bango"
+dictionary['Libr'] = {
+    'atom': "bomb",
+    'nitrous': "oxide",
+}
+dictionary['list'] = ["thingy", "mabobber",]
+
+dictionary['Libr']['add'] = "this thing"
+
+
+class Aaron:
+    age = 30
+    weight = 195
+    sport = "tennis"
+
+
+class Robbie:
+    age = 29
+    weight = 150
+    sport = "baseball"
+
+
+dictionary2 = {
+    'Aaron': Aaron,
+    'Robbie': Robbie,
+}
+
+
+for keys in dictionary:
+    print(keys)
+    print(dictionary[keys])
+
+    if dictionary[keys] == dictionary['Libr']:
+        for thing in dictionary['Libr']:
+            print(thing)
+            print(dictionary['Libr'][thing])
