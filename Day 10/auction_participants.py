c829fb41c4039170bcbb26d717bee632beb14a24
Aaron Jenkins <97716896+Nycepter@users.noreply.github.com>
2023-10-23 23:42:21 -0500

    update

---
+++ Day 10/auction_participants.py
@@ -0,0 +1,14 @@
+auction_participants = {
+}
+
+
+while True:
+    def auction_list(name, bid):
+
+        auction_participants[name] = bid
+        print(auction_participants)
+
+    auction_list("Aaron", 100)
+    auction_list("Dan", 100)
+
+    break
