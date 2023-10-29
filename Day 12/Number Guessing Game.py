c4a118f0e53c55d955ee61d0dbc52e9c47bf16cb
Aaron Jenkins <97716896+Nycepter@users.noreply.github.com>
2023-10-29 00:06:02 -0500

    Update Number Guessing Game.py

--- Day 12/Number Guessing Game.py
+++ Day 12/Number Guessing Game.py
@@ -3,93 +3,48 @@ print("Welcome to the number guessing game! I think of a number from 1 to 100, a
 Score = 0
 correct_guesses = 0
 incorrect_guesses = 0
+
+
+def difficulty(input):
+    if input == "easy":
+        LIVES = 10
+    elif input == "hard":
+        LIVES = 5
+    elif input == "ironman":
+        LIVES = 1
+    return LIVES
+
+
 while True:
-    LIVES = 10
     Win = False
-    difficulty = input(
-        "\nDo you want to play on 'Easy', 'Hard', or 'Ironman'? \n").lower()
+    LIVES = difficulty(input(
+        "\nDo you want to play on 'Easy', 'Hard', or 'Ironman'? \n").lower())
     while LIVES > 0 and Win == False:
-        if difficulty == "easy":
-            Number = random.randrange(1, 101)
-            print(f"Guesses remaining: {LIVES}")
-            while LIVES > 0:
-                Guess = int(input("Please enter your guess. \n\n"))
-                if Guess > Number:
-                    LIVES -= 1
-                    print("Your guess is too high.")
-                    print(f"Guesses remaining: {LIVES}")
-                    continue
-                elif Guess < Number:
-                    LIVES -= 1
-                    print("Your guess is too low.")
-                    print(f"Guesses remaining: {LIVES}")
-                    continue
-                elif Guess == Number:
-                    correct_guesses += 1
-                    Score += 10
-                    print("That is the number, you win!")
-                    print(
-                        f"Score: {Score / (correct_guesses + incorrect_guesses)}")
-                    print(f"Wins: {correct_guesses}")
-                    print(f"Losses: {incorrect_guesses}")
-                    Win = True
-                    break
-            else:
+        Number = random.randrange(1, 101)
+        print(f"Guesses remaining: {LIVES}")
+        while LIVES > 0:
+            Guess = int(input("Please enter your guess. \n\n"))
+            if Guess > Number:
+                LIVES -= 1
+                print("Your guess is too high.")
+                print(f"Guesses remaining: {LIVES}")
                 continue
-        if difficulty == "hard":
-            LIVES = 5
-            Number = random.randrange(1, 101)
-            print(f"Guesses remaining: {LIVES}")
-            while LIVES > 0:
-                Guess = int(input("Please enter your guess. \n\n"))
-                if Guess > Number:
-                    LIVES -= 1
-                    print("Your guess is too high.")
-                    print(f"Guesses remaining: {LIVES}")
-                    continue
-                elif Guess < Number:
-                    LIVES -= 1
-                    print("Your guess is too low.")
-                    print(f"Guesses remaining: {LIVES}")
-                    continue
-                elif Guess == Number:
-                    correct_guesses += 1
-                    Score += 10
-                    print("That is the number, you win!")
-                    print(
-                        f"Score: {Score / (correct_guesses + incorrect_guesses)}")
-                    print(f"Wins: {correct_guesses}")
-                    print(f"Losses: {incorrect_guesses}")
-                    Win = True
-                    break
-            else:
+            elif Guess < Number:
+                LIVES -= 1
+                print("Your guess is too low.")
+                print(f"Guesses remaining: {LIVES}")
                 continue
-        if difficulty == "ironman":
-            LIVES = 1
-            Number = random.randrange(1, 101)
-            print(f"Guesses remaining: {LIVES}")
-            while LIVES > 0:
-                Guess = int(input("Please enter your guess. \n\n"))
-                if Guess > Number:
-                    LIVES -= 1
-                    print("Your guess is too high.")
-                    continue
-                elif Guess < Number:
-                    LIVES -= 1
-                    print("Your guess is too low.")
-                    continue
-                elif Guess == Number:
-                    correct_guesses += 1
-                    Score += 10
-                    print("That is the number, you win!")
-                    print(
-                        f"Score: {Score / (correct_guesses + incorrect_guesses)}")
-                    print(f"Wins: {correct_guesses}")
-                    print(f"Losses: {incorrect_guesses}")
-                    Win = True
-                    break
-            else:
-                continue
+            elif Guess == Number:
+                correct_guesses += 1
+                Score += 10
+                print("That is the number, you win!")
+                print(
+                    f"Score: {Score / (correct_guesses + incorrect_guesses)}")
+                print(f"Wins: {correct_guesses}")
+                print(f"Losses: {incorrect_guesses}")
+                Win = True
+                break
+
     if LIVES > 0 and Win == True:
         continue
 
