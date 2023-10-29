af55a93bb3f8193fa1ae74e547820f9ed1e51d72
Aaron Jenkins <97716896+Nycepter@users.noreply.github.com>
2023-10-29 00:13:38 -0500

    Update Number Guessing Game.py

--- Day 12/Number Guessing Game.py
+++ Day 12/Number Guessing Game.py
@@ -1,8 +1,5 @@
 import random
 print("Welcome to the number guessing game! I think of a number from 1 to 100, and you try to guess it!")
-Score = 0
-correct_guesses = 0
-incorrect_guesses = 0
 
 
 def difficulty(input):
@@ -15,44 +12,51 @@ def difficulty(input):
     return LIVES
 
 
-while True:
-    Win = False
-    LIVES = difficulty(input(
-        "\nDo you want to play on 'Easy', 'Hard', or 'Ironman'? \n").lower())
-    while LIVES > 0 and Win == False:
-        Number = random.randrange(1, 101)
-        print(f"Guesses remaining: {LIVES}")
-        while LIVES > 0:
-            Guess = int(input("Please enter your guess. \n\n"))
-            if Guess > Number:
-                LIVES -= 1
-                print("Your guess is too high.")
-                print(f"Guesses remaining: {LIVES}")
-                continue
-            elif Guess < Number:
-                LIVES -= 1
-                print("Your guess is too low.")
-                print(f"Guesses remaining: {LIVES}")
-                continue
-            elif Guess == Number:
-                correct_guesses += 1
-                Score += 10
-                print("That is the number, you win!")
-                print(
-                    f"Score: {Score / (correct_guesses + incorrect_guesses)}")
-                print(f"Wins: {correct_guesses}")
-                print(f"Losses: {incorrect_guesses}")
-                Win = True
-                break
+def Guessing_Game():
+    Score = 0
+    correct_guesses = 0
+    incorrect_guesses = 0
+    while True:
+        Win = False
+        LIVES = difficulty(input(
+            "\nDo you want to play on 'Easy', 'Hard', or 'Ironman'? \n").lower())
+        while LIVES > 0 and Win == False:
+            Number = random.randrange(1, 101)
+            print(f"Guesses remaining: {LIVES}")
+            while LIVES > 0:
+                Guess = int(input("Please enter your guess. \n\n"))
+                if Guess > Number:
+                    LIVES -= 1
+                    print("Your guess is too high.")
+                    print(f"Guesses remaining: {LIVES}")
+                    continue
+                elif Guess < Number:
+                    LIVES -= 1
+                    print("Your guess is too low.")
+                    print(f"Guesses remaining: {LIVES}")
+                    continue
+                elif Guess == Number:
+                    correct_guesses += 1
+                    Score += 10
+                    print("That is the number, you win!")
+                    print(
+                        f"Score: {Score / (correct_guesses + incorrect_guesses)}")
+                    print(f"Wins: {correct_guesses}")
+                    print(f"Losses: {incorrect_guesses}")
+                    Win = True
+                    break
 
-    if LIVES > 0 and Win == True:
-        continue
+        if LIVES > 0 and Win == True:
+            continue
 
-    else:
-        incorrect_guesses += 1
-        print("You are out of guesses, you lose!\n\n")
-        print(
-            f"Score: {Score / (correct_guesses + incorrect_guesses)}")
-        print(f"Wins: {correct_guesses}")
-        print(f"Losses: {incorrect_guesses}")
-        continue
+        else:
+            incorrect_guesses += 1
+            print("You are out of guesses, you lose!\n\n")
+            print(
+                f"Score: {Score / (correct_guesses + incorrect_guesses)}")
+            print(f"Wins: {correct_guesses}")
+            print(f"Losses: {incorrect_guesses}")
+            continue
+
+
+Guessing_Game()
