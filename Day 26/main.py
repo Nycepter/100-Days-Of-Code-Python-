2e2c310a4a25f473608e5806960a0253763737c1
Aaron Jenkins <97716896+Nycepter@users.noreply.github.com>
2023-12-11 00:22:37 -0600

    Create main.py

---
+++ Day 26/main.py
@@ -0,0 +1,12 @@
+
+names = ["Aelx", "Alexander", "Aaron", "Maranda", "Nycepter", "Pip"]
+
+names2 = [name.upper() for name in names if len(name) > 5]
+
+print(names2)
+
+squared_numbers = [number * number for number in numbers]
+
+numbers = [int(number) for item in list_of_strings]
+
+even_num = [number for number in numbers if number % 2 == 0]
