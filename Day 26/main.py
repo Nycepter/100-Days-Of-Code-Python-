4f85a5b3cb04148297c571778ea065f58da61597
Aaron Jenkins <97716896+Nycepter@users.noreply.github.com>
2024-01-16 21:04:39 -0600

    update

--- Day 26/main.py
+++ Day 26/main.py
@@ -1,23 +1,43 @@
-
-# names = ["Aelx", "Alexander", "Aaron", "Maranda", "Nycepter", "Pip"]
-
-# names2 = [name.upper() for name in names if len(name) > 5]
-
-# # print(names2)
-
-# squared_numbers = [number * number for number in numbers]
-
-# numbers = [int(number) for item in list_of_strings]
-
-# even_num = [number for number in numbers if number % 2 == 0]
+import pandas
 
 
-with open("100-Days-Of-Code-Python-/Day 26/file1.txt", mode="r") as file1:
-    list1 = file1.readlines()
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
 
-with open("100-Days-Of-Code-Python-/Day 26/file2.txt", mode="r") as file2:
-    list2 = file2.readlines()
+# Looping through dictionaries:
+for (key, value) in student_dict.items():
+    # Access key and value
+    pass
 
-both_lists = [int(number) for number in list1 if number in list2]
+student_data_frame = pandas.DataFrame(student_dict)
 
-print(both_lists)
+# Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    # Access index and row
+    # Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+# TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+phonetic_codes = pandas.read_csv(
+    "100-Days-Of-Code-Python-/Day 26/nato_phonetic_alphabet.csv")
+phonetic_codes_dict = phonetic_codes.to_dict()
+df = pandas.DataFrame(phonetic_codes_dict)
+
+
+letterdict = {row.letter: row.code for (index, row) in df.iterrows()}
+
+
+word = input("Enter a word: ").upper()
+
+output = [letterdict[letter] for letter in word]
+
+print(output)
