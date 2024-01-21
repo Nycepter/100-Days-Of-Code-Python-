79c953da32bc780f5dba3d1f10db4186fdb80dac
Aaron Jenkins <97716896+Nycepter@users.noreply.github.com>
2024-01-20 20:49:48 -0600

    update

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("Work In Progress")
+window.minsize(width=500, height=500)
+
+label1 = tkinter.Label(text="Test label 1")
+label1.pack()
+
+
+window.mainloop()
